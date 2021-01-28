bec14fa9a12cb0aad3a715b4f7ad71c36c4d8094
BlaringShelf777 <56661141+BlaringShelf777@users.noreply.github.com>
2021-01-28 16:31:35 -0300

    InsetionSort size defined by COMMAND LINE

--- InsertionSort/main.c
+++ InsertionSort/main.c
@@ -5,30 +5,39 @@
 #include <time.h>
 
 #define SIZE 10
+#define ARGS 2
 
 // Function Prototype
-void makeArray(int array[], int size);
+int *makeArray(int size);
 void printArray(int array[], int size);
 void insertionSort(int array[], int size);
 
-int main(){
-    int array[SIZE] = {0};
+int main(int argc, char *argv[]){
+    if (argc != ARGS){
+        printf("\n> ERROR! usage: <main_file> <array_size>\n\n");
+        return 1;
+    }
+
+    int size = atoi(argv[argc-1]), *array = makeArray(size);
     
-    makeArray(array, SIZE);
-    printArray(array, SIZE);
+    printArray(array, size);
     printf("-------------SORTED--------------\n");
-    insertionSort(array, SIZE);
-    printArray(array, SIZE);
+    insertionSort(array, size);
+    printArray(array, size);
 
     return 0;
 }
 
 // Function Implementation:
 // Creates an array with random integers up to 100 (not inclusive)
-void makeArray(int array[], int size){
+int *makeArray(int size){
+    int *array = (int *) malloc(sizeof(int) * size);
+
     srand(time(NULL));
     for (int i = 0; i < size; i++)
         array[i] = rand() % 100;
+    
+    return array;
 }
 
 // Prints an array of integers of 'int size' length
