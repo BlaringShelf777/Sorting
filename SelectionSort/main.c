640a9751f137f8d053b9ba8a8b4b193f02d1023e
BlaringShelf777 <56661141+BlaringShelf777@users.noreply.github.com>
2021-02-13 20:14:03 -0300

    Sorts an array of integers using the SelectionSort algorithm

--- SelectionSort/main.c
+++ SelectionSort/main.c
@@ -8,12 +8,16 @@
 
 int *makeArray(int size);
 void printArray(int array[], int size);
+void selectionSort(int array[], int size);
 
 
 
 int main(){
     int *array = makeArray(10);
-    
+
+    printArray(array, 10);
+    selectionSort(array, 10);
+    printArray(array, 10);
 
     return 0;
 }
@@ -36,6 +40,17 @@ void printArray(int array[], int size){
     printf("\n");
 }
 
+// SelectionSort
+void selectionSort(int array[], int size){
+    for (int i = 0; i < size; i++){
+        int min = i, aux = array[i];
 
+        for (int j = i + 1; j < size; j++)
+            if (array[min] > array[j])
+                min = j;
+        array[i] = array[min];
+        array[min] = aux;
+    }
+}
 
 
